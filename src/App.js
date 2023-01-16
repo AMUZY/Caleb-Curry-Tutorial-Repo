b053183cc091899916580f1172e28f67349ef9b7
amuzy <nnamdiamu5@gmail.com>
2023-01-16 13:09:23 +0100

    about to go over props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
+import Employee from './components/Employee';
+
+
+const user = {
+  firstname : 'Amu',
+  lastname : 'Nnamdi'
+};
+
+function formatName(user) {
+  return user.firstname + " " + user.lastname;
+}
+
+const emp_check = false;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hey, {formatName(user)}! </h1>
+      { emp_check ? 
+      ( 
+            <>
+            <Employee/>
+            <Employee/>
+            <Employee/>
+            <Employee/>
+            <Employee/>
+          </>
+    ) : (
+      <p> You cannot see the employees </p>
+    )}
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,10 @@
+function Employee () {
+    return <h3>
+        Hey, Employee
+    </h3>
+}
+
+
+
+
+export default Employee
